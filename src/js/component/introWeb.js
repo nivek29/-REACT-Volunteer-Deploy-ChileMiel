b56ca34286448c342a4212a44435606f4bdfca2e
nivek29 <kevinmezamc@gmail.com>
2021-03-31 06:38:13 +0000

    nav letras

---
+++ src/js/component/introWeb.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export const IntroWeb = () => {
+	return (
+		<div id="hero" className="hero">
+			<div className="flex-container container">
+				<div div-block>
+					<h1 className="heading">¿Conoces la importancia de las abejas para el mundo?</h1>
+					<p className="paragraph">
+						¡Setenta mil especies de <strong className="bold-text">plantas</strong> son polinizadas por{" "}
+						<strong>abejas</strong>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
